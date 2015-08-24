f3f6de0fed24104e4a59865724c2527583d5f94d
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2015-08-24 15:14:38 -0700

    added more routes

--- server.js
+++ server.js
@@ -2,16 +2,27 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
+process.env.secret = process.env.secret || 'This is not secure!';
 
 var app = express();
 app.use(express.static(__dirname + '/build'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 var mongoUri = process.env.MONGO_WISHLIST_APP_URI || 'mongodb://localhost/wishlistApp';
 
 var wishlistRoutes = express.Router();
 require('./routes/wishlist-routes')(wishlistRoutes);
 app.use('/api', wishlistRoutes);
+
+var authRoutes = express.Router();
+require('./routes/auth-routes')(authRoutes);
+app.use('/auth', authRoutes);
+
+var userRoutes = express.Router();
+require('./routes/user-routes')(userRoutes);
+app.use('/u', userRoutes);
 
 mongoose.connect(mongoUri, function(err) {
   if (err) console.error('MongoDB connection error:', err);

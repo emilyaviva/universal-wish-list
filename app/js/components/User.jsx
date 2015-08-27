ab80f3538f54c48c6a65ab187e527559ffd9d0a2
Jason Studstill <standstill64@gmail.com>
2015-08-27 00:37:17 -0700

    Add and Delete Items from editable lists

--- app/js/components/User.jsx
+++ app/js/components/User.jsx
@@ -12,7 +12,10 @@ module.exports = React.createClass({
       _id: '',
       creator: '',
       name: '',
-      listItems: []
+      listItems: [],
+      itemName: '',
+      itemUrl: '',
+      itemNotes: ''
     };
   },
   componentDidMount: function() {
@@ -21,7 +24,7 @@ module.exports = React.createClass({
      .end(function(err, res){
         if (res.ok) {
           console.log(res.body);
-          var items = JSON.stringify(res.body.items)
+          var items = JSON.stringify(res.body.items);
           this.setState({
             listItems: this.state.listItems.concat(JSON.parse(items)),
             _id: res.body._id,
@@ -33,22 +36,69 @@ module.exports = React.createClass({
         }
       }.bind(this));
   },
+  handleItemNameChange: function(event) {
+    this.setState({
+      itemName: event.target.value
+    });
+  },
+  handleItemUrlChange: function(event) {
+    this.setState({
+      itemUrl: event.target.value
+    })
+  },
+  handleAddItem: function(e) {
+    e.preventDefault();
+    request
+      .post('/api/w/' + this.state._id + '/items')
+      .send({'description': this.state.itemName, 'url': this.state.itemUrl})
+      .end(function(err, res) {
+        if (res.ok) {
+          alert(JSON.stringify(res.body));
+          this.setState({
+            listItems: this.state.listItems.concat([res.body.data])
+          })
+        } else {
+          alert('Server Error ' + err)
+        }
+      }.bind(this))
+  },
+  handleDelete: function(i, itemId) {
+    console.log(itemId);
+    var updatedListItems = this.state.listItems;
+    updatedListItems.splice(i, 1);
+    request
+      .del('/api/w/' + this.state._id + '/items/' + itemId)
+      .end(function(err, res) {
+        if (res.ok) {
+          alert(JSON.stringify(res.body));
+          this.setState({
+            listItems: updatedListItems
+          })
+        } else {
+          alert('Server Error ' + err)
+        }
+      }.bind(this))
+  },
   render: function() {
+    var itemName = this.state.itemName;
+    var itemUrl = this.state.itemUrl;
+    var handleDelete = this.state.handleDelete;
+
     if (this.state.listItems.length) {
       console.log(this.state);
-      var listItems = this.state.listItems.map(function(item) {
+      var listItems = this.state.listItems.map(function(item, i) {
         return (
-          <article className="article-item">
+          <article className="article-item" >
             <section className="description">
               <h2>{item.description}</h2>
               <a className="view-link" href={item.url}><button>View</button></a>
             </section>
             <section>
-              <button className="btn-delete">Delete Item</button>
+              <button key={item._id} onClick={this.handleDelete.bind(this, i, item._id)} className="btn-delete">Delete Item</button>
             </section>
           </article>
         );
-      });
+      }.bind(this));
     }
     return (
       <main id="user-view">
@@ -65,12 +115,12 @@ module.exports = React.createClass({
 
         <form className="frm-add-item">
           <label>Enter item name:
-            <input name="item-name" className="input-add-item" type="text" placeholder="Item name" />
+            <input value={itemName} onChange={this.handleItemNameChange} name="item-name" className="input-add-item" type="text" placeholder="Item name" />
           </label>
           <label>Enter item url:
-            <input name="item-url" className="input-add-item" type="text" placeholder="Item url" />
+            <input value={itemUrl} onChange={this.handleItemUrlChange} name="item-url" className="input-add-item" type="text" placeholder="Item url" />
           </label>
-          <button id="btn-add-item">Add new</button>
+          <button onClick={this.handleAddItem} id="btn-add-item">Add new</button>
         </form>
 
         {listItems}

0e49e846fe13961844e381db9938c87a46d9f683
Jason Studstill <standstill64@gmail.com>
2015-08-27 20:05:04 -0700

    Style footer and about page

--- app/js/components/About.jsx
+++ app/js/components/About.jsx
@@ -7,21 +7,21 @@ module.exports = React.createClass({
       <main id="about">
         <h1 className="h1-about">About Universal Wishlist</h1>
         <p className="p-about">
-          Welcome to Universal Wishlist! This app will allow users to creat a wishlist
+          Welcome to Universal Wishlist! This app allows users to create a wishlist
           of products and services from any website. From the main page, Create List
-          will allow users to create a new wishlist. This will take you to a page where a
-          user can add items to a wishlist by inputting a url and a name which they would
-          like the item to appear as inside their list.
+          will allow users to create a new wishlist. This will take you to a page where
+          you can add items to a wishlist by inputting a name and url.
         </p>
         <p className="p-about">
           Once a list is created, a user will receive an email with two links: a private
-          link and a public link. the private link will allow users to view existing
-          wishlists. The public link is what they will share with anyone the user would
-          like to send their wishlist too. The public link will allow people to commit to
-          buying a wishlist item.
+          link and a public link. The private link will allow users to view and edit existing
+          wishlists. The public link is meant to be shared with anyone and will allow
+          people to commit to buying a wishlist item.
         </p>
-        <img id="make-wish" src="lib/wish.jpg" />
+        <p className="p-about">
+          Created by Jason Studstill, Emily Aviva Kapor-Mater, Farhad Ahmed
+        </p>
       </main>
-    )
+    );
   }
-})
+});
